935edcbe19fbf6d522af51aefd5eba169bc4f767
TamirisR <Tamis_rodrigues@hotmail.com>
2020-10-27 20:50:45 -0300

    modificando o plugin

--- wordpress/wp-content/plugins/meu_primeiro_plugin/meu_primeiro_plugin.php
+++ wordpress/wp-content/plugins/meu_primeiro_plugin/meu_primeiro_plugin.php
@@ -10,6 +10,22 @@
 */
 
 add_filter( 'login_errors', 'altera_msg_login' );
-function altera_msg_login(){
+function altera_msg_login() {
     return 'Credenciais inválidas!';
+}
+
+add_action('wp_head', 'colocar_og_tags' );
+function colocar_og_tags ()  {
+    if ( is_single ()) {
+        $dados_do_post = get_post( get_the_ID());
+        $nome_do_site= get_bloginfo();
+        $titulo = $dados_do_post->post_tittle;
+        $resumo = $dados_do_post ->post_excerpt; 
+        echo "
+        <meta property='og:tittle' content='". $titulo(). "' >
+        <meta property='og:site_name' content='". $nome_do_site . "' >
+        <meta property='og:url' content='". get_permalink(). "' >
+        <meta property='og:description' content='". $resumo. "' >
+            ";
+    }
 }
